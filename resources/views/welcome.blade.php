3c3cf105a0ffeb9ea3360292159261fd97c9fa6f
Lars Swemmer <larsswemmer@gmail.com>
2017-07-26 02:53:39 +0200

    V1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,93 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>VID-KICK</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+    <body>       
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div class="cover">
+            <img src="http://rec.nl/wp-content/themes/rec/img/vcr.gif" class="effect">
+            <div style="position:absolute; width: 800px;">
+                <img class="img-responsive" style="width: 400px; margin-left: 200px;" src="images/Vid-kick-Logo-White.png">
+                <p class="featured">
+As a personal YouTube Channel Manager, VID-KICK will assist and support you in creating and delivering the best content for you and your fans.</p>
+            </div>
+            <video width="1920" height="1080" autoplay="" muted="" playsinline="" loop="">
+                <source src="http://i.imgur.com/KUC3mkd.mp4" type="video/mp4">
+                {{-- <source src="https://ak8.picdn.net/shutterstock/videos/16692268/preview/stock-footage-different-multimedia-icons-form-a-play-icon-on-grey-background-more-symbols-and-color-backgrounds-a.mp4"> --}}
+            </video>
+        </div>
+
+        <div class="second">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center">
+                        <h2>Build your YouTube channel</h2>
+                        <p>VID-KICK is a YouTube certified professional with a lot of experience in YouTube and video editing. We will help you to build your YouTube Channel.<br /><br />We will take over the tasks you need help with so you can focus on what you do best, creating. Our main goal is to assist and guide you on the path to success<br /><br /><a href="">Get in touch!</a>
+
+                        </p>
+                    </div>
+                </div>
+                <div class="row"></div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="third">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 style="margin-top: 90px;">Channel management</h3>
+                        <p>Keep your channel up-to-date, make it easy to find and irresistible for the viewers. Creating the best upload strategies and schedules that fit your audience. We even include uploading and scheduling your videos.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <video width="100%" autoplay="" muted="" playsinline="" loop="" style="border-radius: 10px;">
+                            <source src="http://i.imgur.com/4WgZSr8.mp4">
+                            
+                        </video>
+                    </div>
+                </div>
+                <div class="row expertise">
+                    <div class="col-md-4">
+                        <h3>Monetizing</h3>
+                        <p>Providing the best options for your uploads to monetize.</p>
+
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Audience Development</h3>
+                        <p>Monitoring your analytics to get the most favourable results.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Content Optimalization</h3>
+                        <p>Supporting you in your productions, editing and designs.</p>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <hr>
+
+        <div class="contact text-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="images/vidkickcontact.jpg">
+                        <h3>Let’s get in touch!</h3>
+                        <p>+31 6 3406 8468<br />
+                        <a href="mailto:contact@vid-kick.com">contact@vid-kick.com</a></p>
+                </div>
+                <div class="col-md-12" style="margin-top: 60px;">
+                    <small>Copyright 2017</small>
+                </div>
+            </div>
+        </div>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
